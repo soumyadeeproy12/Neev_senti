6369e4ecf8f29595eae599a783756d1efceb5818
soumyadeeproy12 <73620372+soumyadeeproy12@users.noreply.github.com>
2022-06-22 14:17:48 +0000

    Update try_1.py

--- try_1.py
+++ try_1.py
@@ -18,11 +18,11 @@ def run():
         number_of_tweets = st.number_input('Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)', 0,50,10)
         submit_button = st.form_submit_button(label='Submit')
         if submit_button:
-        tweets =tw.Cursor(api.search_tweets,q=search_words,lang="en").items(number_of_tweets)
-        tweet_list = [i.text for i in tweets]
-        p = [i for i in classifier(tweet_list)]
-        q=[p[i]['label'] for i in range(len(p))]
-        df = pd.DataFrame(list(zip(tweet_list, q)),columns =['Latest '+str(number_of_tweets)+' Tweets'+' on '+search_words, 'sentiment'])
-        st.write(df)
+            tweets =tw.Cursor(api.search_tweets,q=search_words,lang="en").items(number_of_tweets)
+            tweet_list = [i.text for i in tweets]
+            p = [i for i in classifier(tweet_list)]
+            q=[p[i]['label'] for i in range(len(p))]
+            df = pd.DataFrame(list(zip(tweet_list, q)),columns =['Latest '+str(number_of_tweets)+' Tweets'+' on '+search_words, 'sentiment'])
+            st.write(df)
 if __name__==’__main__’:
     run()        
