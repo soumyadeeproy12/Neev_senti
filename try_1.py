3c330ed639dd5c96ecabed4b52f0230097f58bff
soumyadeeproy12 <73620372+soumyadeeproy12@users.noreply.github.com>
2022-06-22 14:15:06 +0000

    Update try_1.py

--- try_1.py
+++ try_1.py
@@ -13,16 +13,16 @@ classifier = pipeline('sentiment-analysis')
 st.title('Live Twitter Sentiment Analysis with Tweepy and HuggingFace Transformers')
 st.markdown('This app uses tweepy to get tweets from twitter based on the input name/phrase. It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.')
 def run():
-    with st.form(key=’Enter name’):
-        search_words = st.text_input(‘Enter the name for which you want to know the sentiment’)
-        number_of_tweets = st.number_input(‘Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)’, 0,50,10)
-        submit_button = st.form_submit_button(label=’Submit’)
+    with st.form(key='Enter name'):
+        search_words = st.text_input('Enter the name for which you want to know the sentiment')
+        number_of_tweets = st.number_input('Enter the number of latest tweets for which you want to know the sentiment(Maximum 50 tweets)', 0,50,10)
+        submit_button = st.form_submit_button(label='Submit')
         if submit_button:
-        tweets =tw.Cursor(api.search_tweets,q=search_words,lang=”en”).items(number_of_tweets)
+        tweets =tw.Cursor(api.search_tweets,q=search_words,lang="en").items(number_of_tweets)
         tweet_list = [i.text for i in tweets]
         p = [i for i in classifier(tweet_list)]
-        q=[p[i][‘label’] for i in range(len(p))]
-        df = pd.DataFrame(list(zip(tweet_list, q)),columns =[‘Latest ‘+str(number_of_tweets)+’ Tweets’+’ on ‘+search_words, ‘sentiment’])
+        q=[p[i]['label'] for i in range(len(p))]
+        df = pd.DataFrame(list(zip(tweet_list, q)),columns =['Latest '+str(number_of_tweets)+' Tweets'+' on '+search_words, 'sentiment'])
         st.write(df)
 if __name__==’__main__’:
     run()        
